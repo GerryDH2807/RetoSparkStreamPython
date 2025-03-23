03015937077b613b88b6d3f519cfb341cd569268
Gerardo Deustua <gerardeustua@hotmail.com>
2025-03-23 16:57:08 -0600

    Music.py 2 added

--- music.py
+++ music.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
     print("read dataset.csv ... ")
     path_people="dataset.csv"
     df_people = spark.read.csv(path_people,header=True,inferSchema=True)
-    df_people = df_people.withColumnRenamed("date of birth", "birth")
-    df_people.createOrReplaceTempView("people")
-    query='DESCRIBE people'
+    df_people = df_people.withColumnRenamed("year", "fechalanzamiento")
+    df_people.createOrReplaceTempView("music")
+    query='DESCRIBE music'
     spark.sql(query).show(20)
 
-    query="""SELECT name, birth FROM people WHERE sex=="male" ORDER BY `birth`"""
+    query="""SELECT ?Format, fechalanzamiento FROM music WHERE year=="1975" ORDER BY `fechalanzamiento`"""
     df_people_names = spark.sql(query)
     df_people_names.show(20)
 
-    query='SELECT name, `birth` FROM people WHERE `birth` BETWEEN "1903-01-01" AND "1950-12-31" ORDER BY `birth`'
+    query='SELECT name, `fechalanzamiento` FROM music WHERE `fechalanzamiento` BETWEEN "1973" AND "2000" ORDER BY `fechalanzamiento`'
     df_people_1903_1906 = spark.sql(query)
     df_people_1903_1906.show(20)
     results = df_people_1903_1906.toJSON().collect()
@@ -28,8 +28,4 @@ if __name__ == "__main__":
     #df_people_1903_1906.coalesce(1).write.json('results/data_merged.json')
     with open('results/data.json', 'w') as file:
         json.dump(results, file)
-
-    query='SELECT sex,COUNT(sex) FROM people WHERE birth BETWEEN "1903-01-01" AND "1911-12-31" GROUP BY sex'
-    df_people_1903_1906_sex = spark.sql(query)
-    df_people_1903_1906_sex.show()
     spark.stop()
